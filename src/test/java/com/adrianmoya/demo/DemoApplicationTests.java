8cc7de5dc14e796978c03c5bd3f7d5eaef1c9e77
Adrian Moya <adrianmoya@gmail.com>
2024-08-09 17:43:27 -0500

    Initial service

---
+++ src/test/java/com/adrianmoya/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianmoya.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
